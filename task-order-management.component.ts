b9652926d8b1794478f22c0818b442a23ae143c0
Malcolm Olatunji <jolatunj@gmail.com>
2023-01-12 14:42:46 -0500

    changes types in TaskOrderManagement

--- task-order-management.component.ts
+++ task-order-management.component.ts
@@ -3,15 +3,18 @@ import { forkJoin, of } from "rxjs";
 class TaskOrderManagement {
   loadDropDownValues() {
     return forkJoin([
-      of(["source1", "source2"]),
-      of(["price1", "price2"]),
-      of(["office1", "office2"]),
+      of([
+        { isActive: true, name: "timiza" },
+        { isActive: false, name: "unitaid" },
+      ]),
+      of(["cost reimbursable", "labor hour"]),
+      of(["nairobi", "dc"]),
     ]);
   }
 
   initializeTaskOrder(): void {
     this.loadDropDownValues().subscribe(([res1, res2, res3]) => {
-      const fundingSources = res1.map(source => source.toUpperCase());
+      const fundingSources = res1.filter(source => source.isActive);
       const pricings = res2;
       const offices = res3;
     });
